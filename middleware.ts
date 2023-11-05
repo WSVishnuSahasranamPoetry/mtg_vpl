177c8b9133a9145094258f6232a77d9775c92585
VV17CH3R <vvitcherv@gmail.com>
2023-11-05 10:58:35 +0300

    first commit 9

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,74 @@
-import { Database } from '@/types/supabase'
 import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 
+interface Database {
+    public: {
+      Tables: {
+        card4sale: {
+          Row: {
+            class: string
+            created_at: string
+            id: string
+            text: string
+            updated_at: string
+          }
+          Insert: {
+            class: string
+            created_at?: string
+            id?: string
+            text: string
+            updated_at?: string
+          }
+          Update: {
+            class?: string
+            created_at?: string
+            id?: string
+            text?: string
+            updated_at?: string
+          }
+          Relationships: []
+        }
+        wishlistcard: {
+          Row: {
+            created_at: string
+            id: string
+            name: string
+            text: string
+            updated_at: string
+          }
+          Insert: {
+            created_at?: string
+            id?: string
+            name: string
+            text: string
+            updated_at?: string
+          }
+          Update: {
+            created_at?: string
+            id?: string
+            name?: string
+            text?: string
+            updated_at?: string
+          }
+          Relationships: []
+        }
+      }
+      Views: {
+        [_ in never]: never
+      }
+      Functions: {
+        [_ in never]: never
+      }
+      Enums: {
+        [_ in never]: never
+      }
+      CompositeTypes: {
+        [_ in never]: never
+      }
+    }
+  }
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
